a3b40cb834c3a25935fe3c310aea31bd98adf561
Himanshu Bhatt <hbhatt034@gmail.com>
2023-12-04 20:33:52 +0530

    Signup logic with password encryption done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+
+const apiRoutes = require("./routes/index");
+// const {User} = require('./models/index);
+// const bcrypt = require('bcrypt);
 
 const app = express();
 
 const prepareAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, () => {
     console.log(`Server Started on port: ${PORT}`);
+    // const incomingpassword = "123456";
+    // const user = await User.findByPk(3);
+    // const response = bcrypt.compareSync(incomingpassword, user.password);
+    // console.log(response);
   });
 };
 
